cff29ccbdfad3114bfdaaff85df2d12b01c36ecd
batclassroom <148046626+batclassroom@users.noreply.github.com>
2024-06-27 09:13:01 +0200

    correction du message d'erreur dans le login

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
@@ -2,7 +2,7 @@
 const email = document.querySelector("form #email");
 const password = document.querySelector("form #password");
 const form = document.querySelector("form");
-const messageErreur = document.querySelector(".login p");
+const messageErreur = document.querySelector("#emailError");
 let loginLink = document.querySelector("li #longinLink");
 let result;
 
